9685cec01c8f3b59539fd0544eab2826ab3ff313
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2025-01-19 18:53:52 +0100

    api - add basic domain schema

--- apps/api/schema.ts
+++ apps/api/schema.ts
@@ -1,8 +1,29 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
-export const usersTable = sqliteTable("users_table", {
+export const users = sqliteTable("users", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
-  age: int().notNull(),
-  email: text().notNull().unique(),
+  admin: integer({ mode: "boolean" }).default(false),
+  is_email: text().notNull().unique(),
+});
+
+export const offices = sqliteTable("offices", {
+  id: int().primaryKey({ autoIncrement: true }),
+  city: text().notNull(),
+  capacity: integer(),
+  is_peak_limited: integer({ mode: "boolean" }).default(false),
+});
+
+export const reservations = sqliteTable("reservations", {
+  id: int().primaryKey({ autoIncrement: true }),
+  user_id: int("user_id")
+    .notNull()
+    .references(() => users.id),
+  office_id: int("office_id")
+    .notNull()
+    .references(() => offices.id),
+  desk_id: int("desk_id").notNull(),
+  start_time: text("start_time").notNull(), // SQLite stores TIMESTAMP as TEXT
+  end_time: text("end_time").notNull(),
+  status: text("status").notNull().default("active"), // Use TEXT for ENUM-like behavior
 });
